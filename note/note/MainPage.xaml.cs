8c5f368c9a96e13b8c0360ea2559f12cde1e2360
kmac3063 <lantcov.iiu@students.dvfu.ru>
2019-10-17 13:09:49 +1000

    Create new project

---
+++ note/note/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace note
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var page1 = new Page1();
+
+            page1.Disappearing += (a, b) =>
+            {
+                if (page1.text != null)
+                {
+                    Label label = new Label();
+                    label.Text = page1.text;
+                    label.BackgroundColor = Color.Gray;
+                    StackLayout1.Children.Add(label);
+                }
+            };
+            
+            Navigation.PushAsync(page1);
+        }
+    }
+}
---
+++ note/note/Page1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace note
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        public string text = null;
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            text = TextHolder.Text;
+            Navigation.PopAsync();
+        }
+    }
+}
